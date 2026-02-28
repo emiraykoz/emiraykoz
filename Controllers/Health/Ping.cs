720519397da9ecb5f4f06a62015696ff619077ec
emiraykoz <emiray.koz@noxia.cloud>
2026-02-28 03:34:43 +0000

    Add simple health/ping endpoint (returns Pong! in the event of a GET request)

---
+++ Controllers/Health/Ping.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace node_daemon.Controllers.Health
+{
+    [Route("health/ping")]
+    [ApiController]
+    public class Ping : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Content("Pong!", "text/plain");
+    }
+}
